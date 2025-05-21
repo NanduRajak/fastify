63a20c15f5bc0e26aa1045a6302c01e44ff3f6e2
Nandu Rajak <golanukondanandu@gmail.com>
2025-05-22 00:55:35 +0530

    just pushed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
+
 const fastify = require("fastify")({ logger: true });
-const fastifyEnv = require("@fastify/env");
 
 fastify.register(require("@fastify/cors"));
 fastify.register(require("@fastify/sensible"));
@@ -8,7 +8,7 @@ fastify.register(require("@fastify/env"), {
   dotenv: true,
   schema: {
     type: "object",
-    required: ["PORT", "MONGO_DB_URI", "JWT_TOKEN"],
+    required: ["PORT", "MONGODB_URI", "JWT_TOKEN"],
     properties: {
       PORT: { type: "string", default: 3000 },
       MONGO_DB_URI: { type: "string" },
@@ -17,8 +17,41 @@ fastify.register(require("@fastify/env"), {
   },
 });
 
-fastify.get(`/`, function (request, reply) {
+fastify.register(require("./plugin/mangodb"));
+fastify.get("/", function (request, reply) {
   reply.send({ helo: "zoro senpai" });
+});
+
+fastify.get("/test-db", async (request, reply) => {
+  try {
+    const mongoose = fastify.mongoose;
+    const connectionState = mongoose.connection.readyState;
+
+    let status = "";
+    switch (connectionState) {
+      case 0:
+        status = "disconnected";
+        break;
+      case 1:
+        status = "connected";
+        break;
+      case 2:
+        status = "connecting";
+        break;
+      case 3:
+        status = "disconnecting";
+        break;
+
+      default:
+        "unknow";
+        break;
+    }
+    reply.send({ database: status });
+  } catch (err) {
+    fastify.log.error(err);
+    fastify.status(500).reply(err);
+    process.exit(1);
+  }
 });
 
 const start = async () => {
